d30e5593c0f17c5f71e5bf25db65a34330f6d273
Vic9000 <103186101+Vic9000@users.noreply.github.com>
2025-10-28 18:13:28 +0100

    Recept-funktion färdig

--- WhatsForDinner/assets/js/get-recipe.js
+++ WhatsForDinner/assets/js/get-recipe.js
@@ -16,3 +16,22 @@ export async function getRandomRecipe() {
         return [];
     }
 }
+
+export async function searchRecipe(searchTerm) {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        const recipe = json.meals[0];
+        return recipe;
+    } catch (error) {
+        console.error(error.message);
+        window.alert('Something went wrong, recipe could not be found.');
+        return [];
+    }
+}
--- WhatsForDinner/assets/js/make-recipe.js
+++ WhatsForDinner/assets/js/make-recipe.js
@@ -1,23 +1,54 @@
 import { getRandomRecipe } from "./get-recipe.js";
-// import { searchRecipe } from "./get-recipe.js";
+import { searchRecipe } from "./get-recipe.js";
 
-function getIngredients()
+async function makeIngredientList(recipe) {
+    const ingredients = [];
+    const measures = [];
+
+    const ingredientList = document.createElement('ul');
+
+    for (let i = 1; i <= 20; i++) {
+        const ingredientKey = `strIngredient${i}`;
+        const measureKey = `strMeasure${i}`;
+
+        const ingredient = recipe[ingredientKey];
+        const measure = recipe[measureKey];
+        
+        if (ingredient && typeof ingredient === 'string' && ingredient.trim() !== "") {
+            ingredients.push(ingredient.trim());
+        }
+
+        if (measure && typeof measure === 'string' && measure.trim() !== "") {
+            measures.push(measure.trim());
+        }
+    }
+
+    for (let i=0; i < ingredients.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = ` ${measures[i]} ${ingredients[i]}`;
+
+        ingredientList.appendChild(listItem);
+    }
+
+    return ingredientList;
+}
 
 export async function makeRandomRecipe() {
     const recipeContent = await getRandomRecipe();
     const recipeDiv = document.querySelector(".recipe-wrapper");
     console.log(recipeContent);
-    const recipe = document.createElement('div');
-    recipe.className = 'recipe';
 
     const recipeTitle = document.createElement('h3');
     recipeTitle.className = 'recipe-title';
     recipeTitle.textContent = recipeContent.strMeal;
-    recipe.appendChild(recipeTitle);
+    recipeDiv.appendChild(recipeTitle);
 
-    recipeContent.forEach(item => {
-        if (item )
-    });
+    const recipe = document.createElement('div');
+    recipe.className = 'recipe';
+
+    
+    
+    recipe.appendChild(await makeIngredientList(recipeContent));
 
     const instructions = document.createElement('p');
     instructions.textContent = recipeContent.strInstructions;
